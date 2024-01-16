baa26ce4985efa7e612b50290a35d39210ec6a41
tschoemaker <=>
2024-01-16 22:47:54 +0100

    add configuration section and usage subsection

--- masterthesis/figures/chapter4/visit_method_example.js
+++
@@ -1,9 +0,0 @@
-private visitMethod(ctx: RuleContext) {
-    this.returnType = ctx.getChild(0).text
-    this.methodName = ctx.getChild(1).text;
-    let visitor = new MethodParamsAndThrowVisitor();
-    let paramsThrow = visitor.visit(ctx);
-    this.methodParams = paramsThrow.params;
-    this.thrownException = paramsThrow.thrownException;
-    this.methodBody = new MethodBodyTextVisitor().visit(ctx)
-}
