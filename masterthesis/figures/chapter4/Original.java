82d95d4a93061f9264a44cd3c5674322b8ead537
timo <timo.compf@outlook.de>
2024-08-21 16:31:42 +0200

    many changes

---
+++ masterthesis/figures/chapter4/Original.java
@@ -0,0 +1,11 @@
+public class Library {
+    public boolean someLibraryMethod() {
+       System.out.println(sign);
+       System.out.println(mantissa);
+       System.out.println(exponent);
+        return true;
+    }
+    private boolean sign;
+    private double mantissa;
+    private int exponent;
+}
