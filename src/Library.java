dd186a97a9fb73fa508883572a09a953159d556a
timo <timo.compf@outlook.de>
2024-11-24 16:29:14 +0100

    more restructuring

---
+++ src/Library.java
@@ -0,0 +1,26 @@
+public class Library{
+    public void foor(){
+        System.out.println(sign);
+        System.out.println(mantissa);
+        System.out.println(exponent);
+    }
+    private boolean sign;
+    private double mantissa;
+    private int exponent;
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ src/Library2.java
@@ -0,0 +1,8 @@
+public class Library{
+    public void foor(){
+        System.out.println(numProp.getSign());
+        System.out.println(numProp.getMantissa());
+        System.out.println(numProp.getExponent());
+    }
+    private NumProp numProp;
+}
