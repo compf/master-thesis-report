2f859ab1463a283d2d9bdb1e8b03d042a3b3d159
timo <timo.compf@outlook.de>
2024-12-12 11:22:28 +0100

    restructuring

--- masterthesis/figures/chapter5/multiline_method.java
+++
@@ -1,6 +0,0 @@
-  /**
-   * this a test method
-    */
-    void test(int a,
-              int b,
-              int c) {}
--- masterthesis/figures/metrics/null_handling.java
+++
@@ -1,8 +0,0 @@
-/**
- * Assign all field values of the target to the source
- * @param target the target object, may not be null
- * @param source the source object
- */
-void assign(Object target,Object source){
-
-}
